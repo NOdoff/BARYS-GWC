fd08f711180c3913e6dc13d3812f776b7f608a91
shuangela <angelashu103@gmail.com>
2019-07-24 11:04:47 -0700

    js for mouse over

--- BARYS.js
+++ BARYS.js
@@ -1,1 +1,12 @@
 // idk this is the file
+
+function MouseOverHeader(headerID) {
+  
+  var home = document.getElementById(headerID);
+  home.style.display = 'block';
+}
+
+function MouseNotOverHeader(headerID) {
+  var home = document.getElementById(headerID);
+  home.style.display = 'none';
+}
