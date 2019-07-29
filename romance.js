d047f03d1472c18e64cbff4325465791eb656c5b
art3mi5rs <shachaf.smith@gmail.com>
2019-07-29 14:26:42 -0700

    y u hate me?!

--- romance.js
+++ romance.js
@@ -26,7 +26,7 @@ function setup() {
   gameOver = false;
   updateSprites(false);
   background("#000000");
-  fill("#00fff6");
+  fill("#ff4ad8");
   textAlign(CENTER);
   textSize(42);
   text('Click this game and then press "c" to play!', width/2, height/2);
@@ -40,7 +40,7 @@ function draw() {
     }
     if (!GameStart) {
      background("#000000");
-     fill("#00fff6");
+     fill("#ff4ad8");
      textAlign(CENTER);
      textSize(42);
      text('Click this game and then press "c" to play!', width/2, height/2);
@@ -65,7 +65,7 @@ function draw() {
       loveArrows[i].remove();
       }
    background("#000000");
-   fill("#00fff6");
+   fill("#ff4ad8");
    textAlign(CENTER);
    textSize(42)
    text('GAME OVER - Press "c" to try again', width/2, height/2);
@@ -193,4 +193,3 @@ function disappear() {
         }
   }
 }
-
