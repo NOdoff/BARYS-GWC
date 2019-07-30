715b46c544c6f07e89b28365c9908b5a89bd5c87
ritvibhatt <53196324+ritvibhatt@users.noreply.github.com>
2019-07-30 14:24:58 -0700

    Update romance.js

--- romance.js
+++ romance.js
@@ -31,7 +31,6 @@ function setup() {
   textAlign(CENTER);
   textSize(30);
   text('Click this game and then press "c" to play!', width/2, height/2);
-  text('Controls: w for up, s for down', width/4, height/4);
 }
 
 function draw() {
@@ -46,6 +45,7 @@ function draw() {
      textAlign(CENTER);
      textSize(30);
      text('Click this game and then press "c" to play!', width/2, height/2);
+     text('Controls: w for up, s for down', width/4, height/4);
      return;
     }
 
